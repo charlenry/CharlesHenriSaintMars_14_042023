a2592a17f309f90dbac6ede1890977752000afce
Charles-Henri St-Mars <ch.stmars@gmail.com>
2023-04-24 18:15:10 +0200

    Préparation du composant Modal pour npm

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import wealthHealth from '../../assets/img/logo-50x50.jpg';
+import wealthHealth from '../../assets/img/logo.jpg';
 
 /**
  * This is defining a functional component named `Header` that takes in `props` as a parameter. The component returns a JSX element that displays the HRnet logo and title.
